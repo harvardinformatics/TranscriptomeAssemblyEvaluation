ad0233cb380f353b8b9865a382d1ed2f1c607c3c
afreedman <adamfreedman@fas.harvard.edu>
2019-04-02 14:32:18 -0400

    added rel to mid, fixed distance list errors

--- genotyping/python_code/GetSuperTsErrorCount.py
+++ genotyping/python_code/GetSuperTsErrorCount.py
@@ -12,16 +12,18 @@ superts_fasta =  open(sys.argv[3],'r')
 
 error_dict = defaultdict(int)
 d2end_dict = defaultdict(list)
-d2start_dict = defaultdict(list) 
-
+d2start_dict = defaultdict(list)
+reld2mid_dict = defaultdict(list)
+reld2start_dict = defaultdict(list)
+reld2end_dict = defaultdict(list)
 st_length_dict = {}
+
 for record in SeqIO.parse(superts_fasta,'fasta'):
     st_length_dict[record.id] = len(record.seq)
 
-
 fields = fopen.readline().strip().split('\t')
 fout = open('SuperTsErrors_%s.tsv' % sid,'w')
-fout.write('supertsid\tnum_gt_errors\tperr\tdist2end\tdist2start\trel2start\trel2end\n')
+fout.write('supertsid\tnum_gt_errors\tperr\tdist2end\tdist2start\trel2end\trel2start\trel2mid\n')
 for line in fopen:
     linelist = line.strip().split('\t')
     linedict =  dict(zip(fields,linelist))
@@ -32,12 +34,13 @@ for line in fopen:
         if ref_alleles != st_alleles:
             st_error_pos =  int(linedict['supertspositions'].split(':')[1])
             error_dict[linedict['supertspositions'].split(':')[0]]+=1
-            d2end_dict[linedict['supertspositions'].split(':')[0]] = st_length_dict[linedict['supertspositions'].split(':')[0]] - st_error_pos
-            d2start_dict[linedict['supertspositions'].split(':')[0]] = st_error_pos - 1
-
-
+            d2end_dict[linedict['supertspositions'].split(':')[0]].append(st_length_dict[linedict['supertspositions'].split(':')[0]] - st_error_pos)
+            d2start_dict[linedict['supertspositions'].split(':')[0]].append(st_error_pos - 1)
+            reld2mid_dict[linedict['supertspositions'].split(':')[0]].append(abs(0.5-(st_error_pos/float(st_length_dict[linedict['supertspositions'].split(':')[0]]))))
+            reld2start_dict[linedict['supertspositions'].split(':')[0]].append((st_error_pos-1)/float(st_length_dict[linedict['supertspositions'].split(':')[0]]))
+            reld2end_dict[linedict['supertspositions'].split(':')[0]].append((st_length_dict[linedict['supertspositions'].split(':')[0]] - st_error_pos)/float(st_length_dict[linedict['supertspositions'].split(':')[0]]))
 for st in error_dict:
-    fout.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % (st,error_dict[st],error_dict[st]/float(st_length_dict[st]),d2end_dict[st],d2start_dict[st],d2end_dict[st]/float(st_length_dict[st]),d2start_dict[st]/float(st_length_dict[st])))
+    fout.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % (st,error_dict[st],error_dict[st]/float(st_length_dict[st]),median(d2end_dict[st]),median(d2start_dict[st]),median(reld2end_dict[st]),median(reld2start_dict[st]),median(reld2mid_dict[st])))
 
 fout.close()    
     
